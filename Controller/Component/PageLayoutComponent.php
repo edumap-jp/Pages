f667a23ca4b58d1da8a140fc7e0abce13b70f97c
s-nakajima <nakajimashouhei@gmail.com>
2019-03-03 19:05:49 +0900

    読み込みによってはエラーなるため修正（UnitTestで発生）

--- Controller/Component/PageLayoutComponent.php
+++ Controller/Component/PageLayoutComponent.php
@@ -10,7 +10,6 @@
  */
 
 App::uses('Component', 'Controller');
-App::uses('PagesLanguage', 'Pages.Model');
 App::uses('CurrentLibPage', 'NetCommons.Lib/Current');
 
 /**
@@ -59,14 +58,6 @@ class PageLayoutComponent extends Component {
 		if (!self::$_page) {
 			//pathからページデータ取得
 			if (! isset($controller->viewVars['page'])) {
-//				$this->Page = ClassRegistry::init('Pages.Page');
-//				$page = $this->Page->getPageWithFrame(
-//					Current::read('Page.permalink'), Current::read('Space.id')
-//				);
-//				if (empty($page)) {
-//					throw new NotFoundException();
-//				}
-//				self::$_page = $page;
 				self::$_page = CurrentLibPage::getInstance()->findCurrentPage();
 			} else {
 				self::$_page = $controller->viewVars['page'];
@@ -116,6 +107,8 @@ class PageLayoutComponent extends Component {
  * @return void
  */
 	public function setMeta(Controller $controller) {
+		App::uses('PagesLanguage', 'Pages.Model');
+
 		//メタデータのセット
 		$meta['title'] = Hash::get(
 			$controller->viewVars['page'],
